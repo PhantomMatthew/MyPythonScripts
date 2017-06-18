4f181a4352596c8e76c38e4fbe5e4436f18fb0c6
PhantomMatthew <jiabei.yuan@gmail.com>
2017-06-18 22:08:07 +0800

    restructure

--- Get_Network_Info.py
+++ Get_Network_Info.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env Python3
 import socket
+from binascii import hexlify
+
 
 '''Get Local Machine Info'''
 def print_machine_info():
@@ -16,6 +18,18 @@ def get_remote_machine_info():
     except socket.error as err_msg:
         print(remote_host + " error message is: " + err_msg)
 
+ip_addrs = ['127.0.0.1', '192.168.199.204']
+
+def convert_ip4_address():
+    for ip_addr in ip_addrs:
+        packed_ip_addr = socket.inet_aton(ip_addr)
+        unpacked_ip_addr = socket.inet_ntoa(packed_ip_addr)
+        #print("IP Address: " + ip_addr + " => Packed: " + hexlify(packed_ip_addr) + ", Unpacked: " + unpacked_ip_addr)
+        print("IP Address: %s => Packed: %s, Unpacked: %s" % (ip_addr, hexlify(packed_ip_addr), unpacked_ip_addr))
+
+
 if __name__ == '__main__':
     print_machine_info()
     print(help(socket.gethostname))
+    get_remote_machine_info()
+    convert_ip4_address()
